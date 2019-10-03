51488750997c196bb5c0b12a6e92036f92aa4d21
joshtru <joshuaojc@gmail.com>
2019-10-03 03:16:36 -0500

    Updates

---
+++ precache-manifest.8b03d93fb6a274a7bf7faf218a87d041.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "322210677e89c7062db56e76a7be653e",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "8c44bda0f02493f67570",
+    "url": "/weather-app/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "9dc9be8a2dca39874c71",
+    "url": "/weather-app/static/css/main.68ffb582.chunk.css"
+  },
+  {
+    "revision": "8c44bda0f02493f67570",
+    "url": "/weather-app/static/js/2.0b505b20.chunk.js"
+  },
+  {
+    "revision": "9dc9be8a2dca39874c71",
+    "url": "/weather-app/static/js/main.56f2a8b3.chunk.js"
+  },
+  {
+    "revision": "33f16bccc856adc74a41",
+    "url": "/weather-app/static/js/runtime-main.3d21fe14.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/joshtru/weather-app/precache-manifest.9782abe80f51b63575cc450b06b00958.js"
+  "/weather-app/precache-manifest.8b03d93fb6a274a7bf7faf218a87d041.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/joshtru/weather-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
